d7f23805d86ded7f128e7503206c93c255898abb
Tayyab Sabbir <100309512+T4yy4b@users.noreply.github.com>
2024-08-07 18:25:08 +0500

    someCHanges

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+"use client"
+import styles from "@/components/form.module.css"
+function Button() {
+  return (
+    <button type="submit" className={styles.submitButton}>
+    Subscribe
+  </button>
+  )
+}
+
+export default Button
---
+++ src/components/CampaignForm.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import styles from "./form.module.css";
+import Button from "./Button";
+
+const CampaignForm = () => {
+  return (
+    <form className={styles.formContainer}>
+      <div className={styles.imgDiv}>
+      
+        <Image src={"/images/dr.png"} alt="dr" width={100} height={100} />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="name" className={styles.formLabel}>
+          Enter your jazz Number:
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className={styles.formInput}
+          placeholder="Enter your name"
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="email" className={styles.formLabel}>
+          Enter Opt:
+        </label>
+        <div className={styles.formInputContainer}>
+          <input
+            type="number"
+            name="email1"
+            className={styles.formInput}
+          />
+          <input
+            type="number"
+            name="email2"
+            className={styles.formInput}
+          />
+          <input
+            type="number"
+            name="email3"
+            className={styles.formInput}
+          />
+          <input
+            type="number"
+            name="email4"
+            className={styles.formInput}
+          />
+        </div>
+      </div>
+
+      <div className={styles.buttonDiv}><Button/></div>
+    </form>
+  );
+};
+
+export default CampaignForm;
